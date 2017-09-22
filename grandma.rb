7a0cbb0b6d40a23b67301a8569a378c48bfc0efa
Adam Fudge <coolfudge@gmail.com>
2017-09-22 22:19:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string_input)
-  if !(string_input.downcase == string_input.swapcase)
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif string_input == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+  if string_input == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string_input.downcase == string_input.swapcase
+    return "NO, NOT SINCE 1938!"
   else
-    puts "NO, NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
